8f0db83cb3b90df35445bc615a50788dc2b093e3
Aymen Jemi <ajemi@localhost.localdomain>
2019-10-13 04:11:00 +0100

    finish categ

--- app.js
+++ app.js
@@ -1,35 +1,26 @@
 var fs = require('fs');
 
-var data = JSON.parse(fs.readFileSync('assets/hotels.json'));
+var hotels = JSON.parse(fs.readFileSync('assets/hotels.json'));
 var pays = JSON.parse(fs.readFileSync('assets/countries.json'));
 
-data.forEach(item => {
-    if(item.country == 'UK'){
-        item.code = 'GB';
-    }else if(item.country == 'USA'){
-        item.code = 'US';
-    }else {
-        item.code = search(pays,item.country);
+function merge(hotels,pays){
+
+    var re = {};
+    re["pays"] = pays;
+
+    hotels.forEach(item => {
         item.name += ' ,' + item.country + ' ,' + item.city + ' ,' + item.address;
-    }
-    if(!item.code){
         item.code = 'NN';
-    }
-    
-});
-
-
-function search(pays,country){
-    var code;
-    pays.forEach(item => {
-        if(item.name == country){
-            code = item.code;
-            return true;
-        }
     });
 
-    return code;
+    re["hotel"] = hotels;
+
+
+    re = JSON.stringify(re);
+    fs.writeFileSync('assets/data.json',re);
+
 }
 
-let re = JSON.stringify(data);
-fs.writeFileSync('assets/data.json', re);
+
+
+merge(hotels,pays);
